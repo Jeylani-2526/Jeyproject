5f3bd5e922edd341463a7e9e29152d525f7e1312
Jeylani <abdallamuhammed07@gmail.com>
2023-05-19 22:33:49 +0300

    new js document and little updates

---
+++ index.js
@@ -0,0 +1,20 @@
+const EmailEl=document.getElementById("email-el")
+EmailEl.addEventListener("click",function(){
+    document.location.href = 'https://mail.google.com/mail/u/0/#inbox '
+})
+const PhoneEl=document.getElementById("Phone-el")
+PhoneEl.addEventListener("click",function(){
+    document.location.href = '_blank'
+})
+const AsebEl=document.getElementById("Aseb-el")
+AsebEl.addEventListener("click",function(){
+    document.location.href = 'https://www.instagram.com/aseb_22/'
+})
+const extEl=document.getElementById("ext-el")
+extEl.addEventListener("click",function(){
+    document.location.href = 'https://github.com/Jeylani-2526/Lead-Tracker-Extension-/blob/main/README.md'
+})
+
+function showPicture() {
+    let pic = document.getElementById("isboortis-el")
+  } 
